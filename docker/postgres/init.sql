ca47bdf1bdf762263dae0fbb3ae032b5feb193c5
Malaika Gupta <malaika23@gmail.com>
2026-07-03 18:49:02 +0530

    chore: initial repository structure with parent pom, docker-compose, and k8s config

---
+++ docker/postgres/init.sql
@@ -0,0 +1,6 @@
+-- Initialize databases for all microservices
+CREATE DATABASE ecommerce_user;
+CREATE DATABASE ecommerce_product;
+CREATE DATABASE ecommerce_inventory;
+CREATE DATABASE ecommerce_order;
+CREATE DATABASE ecommerce_payment;
